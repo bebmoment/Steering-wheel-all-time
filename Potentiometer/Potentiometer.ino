e1666b2f0f3315ffe1f769420612caaa540bc6a2
DerekY2 <derek.yu869@gmail.com>
2024-04-26 10:40:02 -0400

    map config

--- Potentiometer/Potentiometer.ino
+++ Potentiometer/Potentiometer.ino
@@ -1,13 +1,13 @@
 // Noise filtering
   bool EMA = true; // Exponential Movement Average
-  bool ignoreLSB = false; // ignore least significant bits
+  bool ignoreLSB = true; // ignore least significant bits
 
 //Global Variables
     
   int potentiometerPin = 3;//potentiometer pin #
   int potValue = 0;    //initialization of potentiometer value, equivalent to EMA Y
 
-  int LSB_TO_IGNORE = 2; // # of least significant bits to ignore
+  int LSB_TO_IGNORE = 1; // # of least significant bits to ignore
 
   float EMA_a = 0.6;      //initialization of EMA alpha
   int EMA_S = 0;  //set EMA S for t=1
@@ -43,9 +43,9 @@ void loop(){
   // EMA filtering
   if(EMA){
     EMA_S = (EMA_a*potValue) + ((1-EMA_a)*EMA_S);    //run the EMA
-    Serial.println(EMA_S); //print digital value to serial
+    Serial.println(EMA_S + 512); //print digital value to serial... add 512 as map input range is 512-1023
   }
   else{
-    Serial.println(potValue);
+    Serial.println(potValue + 512);
   }
 }
